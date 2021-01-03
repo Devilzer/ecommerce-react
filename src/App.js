11b499e26117ff7e1b2453d335cc65edc96b7de4
Devilzer <jenadeepak724@gmail.com>
2021-01-03 20:25:00 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className= "header">
+        <div className="heading">
+          Store
+        </div>
+        <div className ="head-buttons">
+          <div className= "add-product">
+            Add Product
+          </div>
+          <div className="cart">
+            Cart
+          </div>
+        </div>
+      </div>
+      <div className="product-page"> 
+
+      </div>
     </div>
   );
 }
