f3b1bbeb29a9d1161bcd7b2cd3245d029d4bd25c
Devilzer <jenadeepak724@gmail.com>
2021-01-05 23:05:23 +0530

    add cart page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Button from '@material-ui/core/Button';
 import Products from "./components/Products";
 import AddProduct from "./components/AddProduct";
 import ProductCard from "./components/ProductCard";
+import Rating from '@material-ui/lab/Rating';
+
 
 function App() {
   return (
@@ -24,8 +26,47 @@ function App() {
 
       {/* <Products/> */}
       {/* <AddProduct/> */}
-      <ProductCard/>
+      {/* <ProductCard/> */}
+      <div className="cart-container">
+          <div className="cart-items">
+            <div className="item">
+              <img src="https://media.wired.com/photos/5bcea2642eea7906bba84c67/master/w_2560%2Cc_limit/iphonexr.jpg"></img>
 
+              <div className="sub-info">
+                <div className="name">
+                  iphone 12
+                </div>
+                <Rating name="read-only" value={3} readOnly />
+                <div className="price">
+                  <span>Rs</span> 49999
+                </div>
+              </div>
+              <div className="qyt-container">
+                <div className="qty">
+                  Quantity
+                </div>
+                <div className="btn">
+                  <Button variant="contained" color="primary">
+                    <i className="fas fa-minus-circle"></i>
+                  </Button>
+                  <span className="cnt">0</span>
+                  <Button variant="contained" color="primary">
+                    <i className="fas fa-plus-circle"></i>
+                  </Button>
+                </div>
+              </div>
+              <div className="delete-cont">
+                <Button variant="contained" color="secondary">
+                  <i className="fas fa-trash-alt"></i>
+                </Button>
+              </div>
+
+            </div>
+          </div>
+          <div className="total">
+             <span>RS</span>&nbsp; 1000000 &nbsp;&nbsp;
+          </div>
+      </div>
 
     </div>
   );
